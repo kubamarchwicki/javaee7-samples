0483727f4e0a347f57752b914e771a5c6ff02be8
kubamarchwicki <kuba@marchwicki.pl>
2014-03-09 23:58:45 +0100

    Method and constructors validation docs

--- validation/methods/src/test/java/org/javaee7/validation/methods/ConstructorParametersInjectionTest.java
+++ validation/methods/src/test/java/org/javaee7/validation/methods/ConstructorParametersInjectionTest.java
@@ -15,7 +15,17 @@ import javax.inject.Inject;
 import javax.validation.ConstraintViolationException;
 
 /**
- * @author Jakub Marchwicki
+ * In this test we show how to validate constuctor parameters, invoked upon bean creation.
+ * 
+ * include::MyBean2[]
+ *
+ * The class under test declares two constructor: default (without parameters - required by the
+ * specification) and a custom, parametrized constructor, used for bean creation.
+ * 
+ * The +MyBean2+ class requires a valid parameter +MyParameter+ with a not null attribute _value_
+ * 
+ * include::MyParameter[]
+ * 
  */
 @RunWith(Arquillian.class)
 public class ConstructorParametersInjectionTest {
@@ -26,19 +36,29 @@ public class ConstructorParametersInjectionTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-	@Deployment
+	/**
+         * A standard Java archive is created
+         */
+        @Deployment
 	public static Archive<?> deployment() {
 		return ShrinkWrap.create(JavaArchive.class)
-                .addClasses(MyBean2.class, MyParameter.class)
+                .addClasses(MyBean2.class, MyParameter.class)           //<1> The archive must contain the actual bean as well as all dependent classes
 				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 	}
 
-	@Test
+	/**
+         * In the test we try to invoke the +MyBean2.getValue()+ method on an 
+         * injected instance of +MyBean2+
+         *
+         * Obviously, upon bean creation, the _value_ attriute is empty hence a
+         * violation exception is raised
+         */
+        @Test
 	public void constructorViolationsWhenNullParameters() {
-        thrown.expect(ConstraintViolationException.class);
-        thrown.expectMessage("javax.validation.constraints.NotNull");
-        thrown.expectMessage("MyBean2.arg0.value");
-		bean.getValue();
+        thrown.expect(ConstraintViolationException.class);              //<1> Standard +ConstraintViolationException+ exception in raised
+        thrown.expectMessage("javax.validation.constraints.NotNull");  
+        thrown.expectMessage("MyBean2.arg0.value");                     //<2> The exception referes to the attribute _value_ of the first parameter (_arg0_) of the +MyBean2+ constructor
+        bean.getValue();                                                //<3> The exception is raised only when the actual method is called. Only then the containers resolved the custom constructor, passes the bean to the client and runs the method. In this case the first step fails (non-default constructor)
 	}
 
 }
--- validation/methods/src/test/java/org/javaee7/validation/methods/MethodParametersConstraintsTest.java
+++ validation/methods/src/test/java/org/javaee7/validation/methods/MethodParametersConstraintsTest.java
@@ -21,7 +21,7 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 /**
- * @author Jakub Marchwicki
+ * In this sample we are showing method validation capabilities for JEE7
  */
 @RunWith(Arquillian.class)
 public class MethodParametersConstraintsTest {
@@ -32,25 +32,43 @@ public class MethodParametersConstraintsTest {
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
 
+        /** 
+         * Only bean in requierd for deployment. All constraints and validators
+         * are included in the server runtime
+         */ 
 	@Deployment
 	public static Archive<?> deployment() {
 		return ShrinkWrap.create(JavaArchive.class).addClasses(MyBean.class)
 				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 	}
 
+        /** 
+         * At first +@Size+ constraint is checked for 
+         * 
+         * include::MyBean#sayHello[]
+         */ 
 	@Test
 	public void methodSizeTooLong() {
-		thrown.expect(ConstraintViolationException.class);
-		thrown.expectMessage("javax.validation.constraints.Size");
-		thrown.expectMessage("org.javaee7.validation.methods.MyBean.sayHello");
+		thrown.expect(ConstraintViolationException.class);                      //<1> +ConstraintViolationException+ is thrown
+		thrown.expectMessage("javax.validation.constraints.Size");              //<2> Parameter size is exceeded
+		thrown.expectMessage("org.javaee7.validation.methods.MyBean.sayHello"); //<3> For a method, which a fully qualified name is included in the exception message
 		bean.sayHello("Duke");
 	}
 
+        /**
+         * Testing the same method with a shorter parameter works fine
+         */
 	@Test
 	public void methodSizeOk() {
 		bean.sayHello("Duk");
 	}
 
+        /**
+         * Another constraint checked is +@Future+ which requires that a +Date+ returned 
+         * by method is a future date
+         *
+         * include::MyBean#showDate[]
+         */
 	@Test
 	public void showDateFromPast() {
 		thrown.expect(ConstraintViolationException.class);
@@ -59,30 +77,48 @@ public class MethodParametersConstraintsTest {
 		bean.showDate(false);
 	}
 
+        /**
+         * Similarly, forcing a return of a future date works smoothly.
+         */
 	@Test
 	public void showDateFromFuture() {
 		bean.showDate(true);
 	}
 
+        /**
+         * Finally, a methods parameter can be annotated with more than one constraint
+         * and each parameter can be constrained individually
+         *
+         * include::MyBean#showList[]
+         * 
+         * An empty list is passed 
+         */
 	@Test
 	public void multipleParametersWithEmptyList() {
 		thrown.expect(ConstraintViolationException.class);
-		thrown.expectMessage("javax.validation.constraints.Size");
-		thrown.expectMessage("showList.arg0");
+		thrown.expectMessage("javax.validation.constraints.Size");      //<1> The Size constraint is not valid
+		thrown.expectMessage("showList.arg0");                          //<2> For the first argument of +showList+ method
 		bean.showList(new ArrayList<String>(), "foo");
 	}
 
-	@Test
+	/**
+         * In a similar fashion a second parameter can be checked.
+         * 
+         */
+        @Test
 	public void multipleParametersNullSecondParameter() {
 		thrown.expect(ConstraintViolationException.class);
 		thrown.expectMessage("javax.validation.constraints.NotNull");
-		thrown.expectMessage("showList.arg1");
+		thrown.expectMessage("showList.arg1");                          //<1> Assertion failed for the method's second argument
 
 		List<String> list = new ArrayList<>();
 		list.add("bar");
-		bean.showList(list, null);
+		bean.showList(list, null);                                      //<2> When a _null_ value has been passed 
 	}
 
+        /**
+         * For a non-empty list and not null second parameter, test successfully passed
+         */
 	@Test
 	public void multipleParametersWithCorrectValues() {
 		List<String> list = new ArrayList<>();
